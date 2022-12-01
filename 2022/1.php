7c0588ae805020b7bc5b6dab34b71d90047fab72
mnlcz <manuel.lopez.cosmitz@gmail.com>
2022-12-01 20:55:53 -0300

    Added day 1 part 2

--- 2022/1.php
+++ 2022/1.php
@@ -25,8 +25,34 @@ function part1($in)
             $max = $calories_sum;
         }
     }
-    echo "Max: $max by elf $elf";
+    echo "Part 1: max $max by elf $elf\n";
+}
+
+function part2($in)
+{
+    $arr = parse_into_string_arr($in);
+    $len = count($arr);
+    $map = [];
+    $out = 0;
+    for ($i = 0; $i < $len; $i++)
+    {  
+        $values = array_map('intval', explode("-", $arr[$i]));
+        $calories_sum = array_sum($values);
+        $map[$i] = $calories_sum; // map of INT(index) => INT(sum)
+    }
+    arsort($map); // sort map desc
+    $it = (new ArrayObject($map))->getIterator(); // iterator is needed to iterate the map following the new placements of the keys
+    $counter = 0;
+    foreach ($it as $v) // iterate 3 times to get final result
+    {
+        if ($counter === 3)
+            break;
+        $out += $v;
+        $counter++;
+    }
+    echo "Part 2: $out";
 }
 
 $in = file_get_contents("./inputs/1.txt");
 part1($in);
+part2($in);
--- README.md
+++ README.md
@@ -3,4 +3,7 @@ My advent of code solutions in PHP.
 ## 2022
 - Day 1:
   - [x] Part 1
+  - [x] Part 2
+- Day 2:
+  - [ ] Part 1
   - [ ] Part 2
