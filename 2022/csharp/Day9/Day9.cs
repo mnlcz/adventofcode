d1392ee31ef04addadbd97003302199501a6a481
mnlcz <manuel.lopez.cosmitz@gmail.com>
2022-12-11 03:29:58 -0300

    Completed dar 9 part 2

--- 2022/csharp/Day9/Day9.cs
+++ 2022/csharp/Day9/Day9.cs
@@ -3,6 +3,7 @@
 var input = Parser.IntoArrayMultiSeparator("9", "\n", " ").Select(s => (s[0][0], int.Parse(s[1])));
 var h = new Point(0, 0);
 var t = new Point(0, 0);
+var knots = new Point[10];
 var directions = new Dictionary<char, Point>
 {
     ['R'] = new(1, 0),
@@ -11,6 +12,7 @@ var directions = new Dictionary<char, Point>
     ['D'] = new(0, -1)
 };
 Part1(input);
+Part2(input);
 
 void Part1(IEnumerable<(char, int)> input)
 {
@@ -28,6 +30,24 @@ void Part1(IEnumerable<(char, int)> input)
     Console.WriteLine($"Part 1: {visited.Count}");
 }
 
+void Part2(IEnumerable<(char, int)> input)
+{
+    h = new Point(0, 0);
+    t = new Point(0, 0);
+    var visited = new HashSet<Point> { knots[^1] };
+    foreach (var m in input)
+    {
+        var dir = directions[m.Item1];
+        var n = m.Item2;
+        for (var i = 0; i < n; i++)
+        {
+            MoveWithKnots(dir);
+            visited.Add(knots[^1]);
+        }
+    }
+    Console.Write($"Part 1: {visited.Count}");
+}
+
 static bool AreTouching(Point p1, Point p2)
 {
     return p1.Chebyshev(p2) <= 1;
@@ -42,6 +62,24 @@ void Move(Point direction)
         var newY = h.y == t.y ? 0 : (h.y - t.y) / Math.Abs(h.y - t.y);
         t.x += newX;
         t.y += newY;
+    }
+}
+
+void MoveWithKnots(Point direction)
+{
+    knots![0] += direction;
+    for (var i = 1; i < 10; i++)
+    {
+        h = knots[i - 1];
+        t = knots[i];
+        if (!AreTouching(h, t))
+        {
+            var newX = h.x == t.x ? 0 : (h.x - t.x) / Math.Abs(h.x - t.x);
+            var newY = h.y == t.y ? 0 : (h.y - t.y) / Math.Abs(h.y - t.y);
+            t.x += newX;
+            t.y += newY;
+        }
+        knots[i] = t;
     }
 }
 
