050300a257b0aadb94d97642dfc06f1c1d17843b
mnlcz <manuel.lopez.cosmitz@gmail.com>
2022-12-15 06:58:47 -0300

    New general features

--- utils/csharp/Utils/Common.cs
+++ utils/csharp/Utils/Common.cs
@@ -4,10 +4,17 @@ public record struct Point(int x, int y)
 {
     public override string ToString() => $"x: {x}, y: {y}";
 
-    public static Point operator +(Point one, Point other)
-    {
-        return new(one.x + other.x, one.y + other.y);
-    }
+    public static Point operator +(Point one, Point other) => new(one.x + other.x, one.y + other.y);
 
     public int Chebyshev(Point other) => Math.Max(Math.Abs(x - other.x), Math.Abs(y - other.y));
+
+    public Point Move(Point direction) => this + direction;
+}
+
+public readonly record struct Direction
+{
+    public static Point Right { get; } = new(1, 0);
+    public static Point Left { get; } = new(-1, 0);
+    public static Point Up { get; } = new(0, 1);
+    public static Point Down { get; } = new(0, -1);
 }
