c4c1eddab54fa9e79910ef08645f21c4d85c3a9c
mnlcz <manuel.lopez.cosmitz@gmail.com>
2022-12-14 22:48:37 -0300

    Day 12 template

---
+++ 2022/csharp/Day12/Day12.cs
@@ -0,0 +1,19 @@
+﻿using Utils;
+
+/*
+ * S            => my location      => a
+ * E            => destination      => z
+ * Elevation    => a..z             => lowest to highest
+ * Movement     => only to 1 higher elevation
+ */
+
+
+var input = Parser.Raw("12");
+
+// Build matrix
+
+// Implement logic for movement
+
+// Execute movement
+
+// Loop until destination
