734e743291056c5a889df73a1cf39ecdddd0106e
mnlcz <manuel.lopez.cosmitz@gmail.com>
2023-01-03 11:19:36 -0300

    Setup using graph

--- 2022/csharp/Day12/Day12.cs
+++ 2022/csharp/Day12/Day12.cs
@@ -1,19 +1,53 @@
-﻿using Utils;
-
-/*
- * S            => my location      => a
- * E            => destination      => z
- * Elevation    => a..z             => lowest to highest
- * Movement     => only to 1 higher elevation
- */
+﻿using System.Net;
+using Utils;
 
 
-var input = Parser.Raw("12");
+var input = Parser.IntoGrid("12");
+var (graph, start, end) = Setup(input);
+Writer.Show(input);
 
-// Build matrix
+static (Dictionary<Point, List<Point>>, Point, Point) Setup(Dictionary<Point, char> input) =>
+(
+    BuildGraph(input),
+    input
+        .Where(kv => kv.Value == 'S')
+        .Select(kv => kv.Key)
+        .First(),
+    input
+        .Where(kv => kv.Value == 'E')
+        .Select(kv => kv.Key)
+        .First()
+);
 
-// Implement logic for movement
+static Dictionary<Point, List<Point>> BuildGraph(Dictionary<Point, char> grid)
+{
+    var graph = new Dictionary<Point, List<Point>>();
+    var h = grid
+        .Keys
+        .Select(k => k.y)
+        .Max();
+    var l = grid
+        .Keys
+        .Select(k => k.x)
+        .Max();
 
-// Execute movement
+    for (var i = 0; i <= h; i++)
+    {
+        for (var j = 0; j <= l; j++)
+        {
+            var neighbours = new List<Point>();
+            var current = new Point(j, i);
+            var moves = new[] { Movement.Up, Movement.Down, Movement.Left, Movement.Right };
+            foreach (var m in moves)
+            {
+                var n = current.Move(m);
+                if (n.x >= 0 && n.x <= l && n.y >= 0 && n.y <= h)
+                    neighbours.Add(n);
 
-// Loop until destination
+            }
+            graph[current] = neighbours;
+        }
+    }
+
+    return graph;
+}
