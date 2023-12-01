1a962cbf8229ec843efac27caab138cfbba10e28
mnlcz <manuel.lopez.cosmitz@gmail.com>
2023-12-01 18:37:11 -0300

    Day 3 generated with the script

---
+++ 2023/csharp/Day3/Day3.cs
@@ -0,0 +1,16 @@
+using Utils;
+
+namespace Day3;
+
+public sealed class Solution3 : ISolution
+{
+    public string Part1(string filename)
+    {
+        throw new NotImplementedException();
+    }
+
+    public string Part2(string filename)
+    {
+        throw new NotImplementedException();
+    }
+}
