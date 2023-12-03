fc792f01e6386eb77a72177b511998506288f7ad
mnlcz <manuel.lopez.cosmitz@gmail.com>
2023-12-03 19:38:17 -0300

    Day 3 Part 1 OK

--- 2023/csharp/Day3/Day3.cs
+++ 2023/csharp/Day3/Day3.cs
@@ -4,16 +4,59 @@ namespace Day3;
 
 public sealed class Solution3 : ISolution
 {
-	private string[] Input(string filename)
-	{
-		return new Parser("2023").IntoArray(filename);
-	}
+	private static string[] Input(string filename) => new Parser("2023").IntoArray(filename);
 
 	public string Part1(string filename)
 	{
 		var input = Input(filename);
-		Writer.Show(input);
-		return "TODO";
+		List<int> nums = [];
+		Span<char> num = ['.', '.', '.'];
+		var isPart = false;
+		static bool isSymbol(char c) => !char.IsNumber(c) && c != '.';
+		for (var i = 0; i < input.Length; i++)
+		{
+			for (var j = 0; j < input[i].Length; j++)
+			{
+				var character = input[i][j];
+				if (char.IsNumber(character))
+				{
+					// Check part
+					if (OutOfBoundsCheck.Up(i))
+						if (isSymbol(input[i - 1][j])) isPart = true;
+					if (OutOfBoundsCheck.Down(i, input.Length))
+						if (isSymbol(input[i + 1][j])) isPart = true;
+					if (OutOfBoundsCheck.Left(j))
+						if (isSymbol(input[i][j - 1])) isPart = true;
+					if (OutOfBoundsCheck.Right(j, input[i].Length))
+						if (isSymbol(input[i][j + 1])) isPart = true;
+					if (OutOfBoundsCheck.UpLeft(i, j))
+						if (isSymbol(input[i - 1][j - 1])) isPart = true;
+					if (OutOfBoundsCheck.UpRight(i, j, input[i].Length))
+						if (isSymbol(input[i - 1][j + 1])) isPart = true;
+					if (OutOfBoundsCheck.DownLeft(i, j, input.Length))
+						if (isSymbol(input[i + 1][j - 1])) isPart = true;
+					if (OutOfBoundsCheck.DownRight(i, j, input.Length, input[i].Length))
+						if (isSymbol(input[i + 1][j + 1])) isPart = true;
+
+					if (num[0] == '.') num[0] = character;
+					else if (num[1] == '.') num[1] = character;
+					else num[2] = character;
+				}
+				else
+				{
+					if (num[0] == '.') continue;
+					var str = num[0].ToString();
+					if (num[1] != '.') str += num[1];
+					if (num[2] != '.') str += num[2];
+					if (isPart)
+						nums.Add(int.Parse(str));
+					isPart = false;
+					num.Fill('.');
+				}
+			}
+		}
+
+		return nums.Sum().ToString();
 	}
 
 	public string Part2(string filename)
