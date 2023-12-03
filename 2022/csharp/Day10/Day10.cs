11a27ad3e67ac9324503b75d1e6e4a80386c3dfa
mnlcz <manuel.lopez.cosmitz@gmail.com>
2023-12-03 13:45:54 -0300

    Visual Studio code cleanup

--- 2022/csharp/Day10/Day10.cs
+++ 2022/csharp/Day10/Day10.cs
@@ -6,96 +6,96 @@ Part2(input);
 
 static void Part1(List<List<string>> input)
 {
-    var x = 1;
-    var cycle = 0;
-    var signals = new List<int>();
-    var prev = -20;
-    foreach (var l in input)
-    {
-        if (l is [_, var n])
-            Addx(ref x, ref cycle, int.Parse(n), signals, ref prev);
-        else
-            Noop(x, ref cycle, signals, ref prev);
-    }
-    Console.WriteLine($"Part 1: {signals.Sum()}");
+	var x = 1;
+	var cycle = 0;
+	var signals = new List<int>();
+	var prev = -20;
+	foreach (var l in input)
+	{
+		if (l is [_, var n])
+			Addx(ref x, ref cycle, int.Parse(n), signals, ref prev);
+		else
+			Noop(x, ref cycle, signals, ref prev);
+	}
+	Console.WriteLine($"Part 1: {signals.Sum()}");
 }
 
 static void Part2(List<List<string>> input)
 {
-    var spriteMidPx = 1;
-    var row = "";
-    var crt = new List<string>();
-    foreach (var l in input)
-    {
-        if (l is [_, var n])
-            Addx2(crt, ref row, ref spriteMidPx, int.Parse(n));
-        else
-            Noop2(crt, ref row, ref spriteMidPx);
-    }
-    Console.WriteLine("Part 2:");
-    Render(crt);
+	var spriteMidPx = 1;
+	var row = "";
+	var crt = new List<string>();
+	foreach (var l in input)
+	{
+		if (l is [_, var n])
+			Addx2(crt, ref row, ref spriteMidPx, int.Parse(n));
+		else
+			Noop2(crt, ref row, ref spriteMidPx);
+	}
+	Console.WriteLine("Part 2:");
+	Render(crt);
 }
 
 static void Addx(ref int x, ref int cycle, int n, List<int> signals, ref int prev)
 {
-    cycle++;
-    Check(x, cycle, signals, ref prev);
-    cycle++;
-    Check(x, cycle, signals, ref prev);
-    x += n;
+	cycle++;
+	Check(x, cycle, signals, ref prev);
+	cycle++;
+	Check(x, cycle, signals, ref prev);
+	x += n;
 }
 
 static void Noop(int x, ref int cycle, List<int> signals, ref int prev)
 {
-    cycle++;
-    Check(x, cycle, signals, ref prev);
+	cycle++;
+	Check(x, cycle, signals, ref prev);
 }
 
 static void Check(int x, int cycle, List<int> signals, ref int prev)
 {
-    if (cycle == prev + 40)
-    {
-        signals.Add(cycle * x);
-        prev = cycle;
-    }
+	if (cycle == prev + 40)
+	{
+		signals.Add(cycle * x);
+		prev = cycle;
+	}
 }
 
 static void Addx2(List<string> crt, ref string row, ref int spriteMidPx, int n)
 {
-    Draw(ref row, spriteMidPx);
-    CheckRow(crt, ref row);
-    Draw(ref row, spriteMidPx);
-    CheckRow(crt, ref row);
-    spriteMidPx += n;
+	Draw(ref row, spriteMidPx);
+	CheckRow(crt, ref row);
+	Draw(ref row, spriteMidPx);
+	CheckRow(crt, ref row);
+	spriteMidPx += n;
 }
 
 static void Noop2(List<string> crt, ref string row, ref int spriteMidPx)
 {
-    Draw(ref row, spriteMidPx);
-    CheckRow(crt, ref row);
+	Draw(ref row, spriteMidPx);
+	CheckRow(crt, ref row);
 }
 
 static void Draw(ref string row, int sprite) =>
-    row += IsRowInSprite(row.Length, sprite) ? "#" : ".";
+	row += IsRowInSprite(row.Length, sprite) ? "#" : ".";
 
 static bool IsRowInSprite(int i, int sprite) =>
-    sprite == i || sprite - 1 == i || sprite + 1 == i;
+	sprite == i || sprite - 1 == i || sprite + 1 == i;
 
 static void CheckRow(List<string> crt, ref string row)
 {
-    if (row.Length == 40)
-    {
-        crt.Add(row);
-        row = "";
-    }
+	if (row.Length == 40)
+	{
+		crt.Add(row);
+		row = "";
+	}
 }
 
 static void Render(List<string> crt)
 {
-    foreach (var l in crt)
-    {
-        foreach (var c in l)
-            Console.Write(c == '#' ? "#" : " ");
-        Console.WriteLine();
-    }
+	foreach (var l in crt)
+	{
+		foreach (var c in l)
+			Console.Write(c == '#' ? "#" : " ");
+		Console.WriteLine();
+	}
 }
--- 2022/csharp/Day9/Day9.cs
+++ 2022/csharp/Day9/Day9.cs
@@ -7,10 +7,10 @@ var t = new Point(0, 0);
 var knots = new Point[10];
 var directions = new Dictionary<char, Point>
 {
-    ['R'] = new(1, 0),
-    ['U'] = new(0, 1),
-    ['L'] = new(-1, 0),
-    ['D'] = new(0, -1)
+	['R'] = new(1, 0),
+	['U'] = new(0, 1),
+	['L'] = new(-1, 0),
+	['D'] = new(0, -1)
 };
 
 Part1(input);
@@ -18,70 +18,70 @@ Part2(input);
 
 void Part1(IEnumerable<(char, int)> input)
 {
-    var visited = new HashSet<Point> { t };
-    foreach (var m in input)
-    {
-        var dir = directions[m.Item1];
-        var n = m.Item2;
-        for (var i = 0; i < n; i++)
-        {
-            Move(dir);
-            visited.Add(t);
-        }
-    }
-    Console.WriteLine($"Part 1: {visited.Count}");
+	var visited = new HashSet<Point> { t };
+	foreach (var m in input)
+	{
+		var dir = directions[m.Item1];
+		var n = m.Item2;
+		for (var i = 0; i < n; i++)
+		{
+			Move(dir);
+			visited.Add(t);
+		}
+	}
+	Console.WriteLine($"Part 1: {visited.Count}");
 }
 
 void Part2(IEnumerable<(char, int)> input)
 {
-    h = new Point(0, 0);
-    t = new Point(0, 0);
-    var visited = new HashSet<Point> { knots[^1] };
-    foreach (var m in input)
-    {
-        var dir = directions[m.Item1];
-        var n = m.Item2;
-        for (var i = 0; i < n; i++)
-        {
-            MoveWithKnots(dir);
-            visited.Add(knots[^1]);
-        }
-    }
-    Console.Write($"Part 1: {visited.Count}");
+	h = new Point(0, 0);
+	t = new Point(0, 0);
+	var visited = new HashSet<Point> { knots[^1] };
+	foreach (var m in input)
+	{
+		var dir = directions[m.Item1];
+		var n = m.Item2;
+		for (var i = 0; i < n; i++)
+		{
+			MoveWithKnots(dir);
+			visited.Add(knots[^1]);
+		}
+	}
+	Console.Write($"Part 1: {visited.Count}");
 }
 
 static bool AreTouching(Point p1, Point p2)
 {
-    return p1.Chebyshev(p2) <= 1;
+	return p1.Chebyshev(p2) <= 1;
 }
 
 void Move(Point direction)
 {
-    h += direction;
-    if (!AreTouching(h, t))
-    {
-        var newX = h.x == t.x ? 0 : (h.x - t.x) / Math.Abs(h.x - t.x);
-        var newY = h.y == t.y ? 0 : (h.y - t.y) / Math.Abs(h.y - t.y);
-        t.x += newX;
-        t.y += newY;
-    }
+	h += direction;
+	if (!AreTouching(h, t))
+	{
+		var newX = h.x == t.x ? 0 : (h.x - t.x) / Math.Abs(h.x - t.x);
+		var newY = h.y == t.y ? 0 : (h.y - t.y) / Math.Abs(h.y - t.y);
+		t.x += newX;
+		t.y += newY;
+	}
 }
 
 void MoveWithKnots(Point direction)
 {
-    knots![0] += direction;
-    for (var i = 1; i < 10; i++)
-    {
-        h = knots[i - 1];
-        t = knots[i];
-        if (!AreTouching(h, t))
-        {
-            var newX = h.x == t.x ? 0 : (h.x - t.x) / Math.Abs(h.x - t.x);
-            var newY = h.y == t.y ? 0 : (h.y - t.y) / Math.Abs(h.y - t.y);
-            t.x += newX;
-            t.y += newY;
-        }
-        knots[i] = t;
-    }
+	knots![0] += direction;
+	for (var i = 1; i < 10; i++)
+	{
+		h = knots[i - 1];
+		t = knots[i];
+		if (!AreTouching(h, t))
+		{
+			var newX = h.x == t.x ? 0 : (h.x - t.x) / Math.Abs(h.x - t.x);
+			var newY = h.y == t.y ? 0 : (h.y - t.y) / Math.Abs(h.y - t.y);
+			t.x += newX;
+			t.y += newY;
+		}
+		knots[i] = t;
+	}
 }
 
