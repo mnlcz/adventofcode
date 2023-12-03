4c2734fb8b3f0667f5b46643c26ac2d7e95d55ff
mnlcz <manuel.lopez.cosmitz@gmail.com>
2023-12-03 14:54:26 -0300

    Day 4 created with the new script version

---
+++ 2023/csharp/Day4/Day4.cs
@@ -0,0 +1,16 @@
+using Utils;
+
+namespace Day4;
+
+public sealed class Solution4 : ISolution
+{
+    public string Part1(string filename)
+    {
+        throw new NotImplementedException();
+    }
+
+    public string Part2(string filename)
+    {
+        throw new NotImplementedException();
+    }
+}
