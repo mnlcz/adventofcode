40811aec209784544c5a61b433d60854c1d69ac5
mnlcz <manuel.lopez.cosmitz@gmail.com>
2023-12-04 14:09:42 -0300

    Fails part 2

--- 2023/csharp/Day3/Day3.cs
+++ 2023/csharp/Day3/Day3.cs
@@ -12,7 +12,6 @@ public sealed class Solution3 : ISolution
 		List<int> nums = [];
 		Span<char> num = ['.', '.', '.'];
 		var isPart = false;
-		static bool isSymbol(char c) => !char.IsNumber(c) && c != '.';
 		for (var i = 0; i < input.Length; i++)
 		{
 			for (var j = 0; j < input[i].Length; j++)
@@ -20,23 +19,22 @@ public sealed class Solution3 : ISolution
 				var character = input[i][j];
 				if (char.IsNumber(character))
 				{
-					// Check part
-					if (OutOfBoundsCheck.Up(i))
-						if (isSymbol(input[i - 1][j])) isPart = true;
-					if (OutOfBoundsCheck.Down(i, input.Length))
-						if (isSymbol(input[i + 1][j])) isPart = true;
-					if (OutOfBoundsCheck.Left(j))
-						if (isSymbol(input[i][j - 1])) isPart = true;
-					if (OutOfBoundsCheck.Right(j, input[i].Length))
-						if (isSymbol(input[i][j + 1])) isPart = true;
-					if (OutOfBoundsCheck.UpLeft(i, j))
-						if (isSymbol(input[i - 1][j - 1])) isPart = true;
-					if (OutOfBoundsCheck.UpRight(i, j, input[i].Length))
-						if (isSymbol(input[i - 1][j + 1])) isPart = true;
-					if (OutOfBoundsCheck.DownLeft(i, j, input.Length))
-						if (isSymbol(input[i + 1][j - 1])) isPart = true;
-					if (OutOfBoundsCheck.DownRight(i, j, input.Length, input[i].Length))
-						if (isSymbol(input[i + 1][j + 1])) isPart = true;
+					if (NotOutOfBoundsCheck.Up(i))
+						if (IsSymbol(input[i - 1][j])) isPart = true;
+					if (NotOutOfBoundsCheck.Down(i, input.Length))
+						if (IsSymbol(input[i + 1][j])) isPart = true;
+					if (NotOutOfBoundsCheck.Left(j))
+						if (IsSymbol(input[i][j - 1])) isPart = true;
+					if (NotOutOfBoundsCheck.Right(j, input[i].Length))
+						if (IsSymbol(input[i][j + 1])) isPart = true;
+					if (NotOutOfBoundsCheck.UpLeft(i, j))
+						if (IsSymbol(input[i - 1][j - 1])) isPart = true;
+					if (NotOutOfBoundsCheck.UpRight(i, j, input[i].Length))
+						if (IsSymbol(input[i - 1][j + 1])) isPart = true;
+					if (NotOutOfBoundsCheck.DownLeft(i, j, input.Length))
+						if (IsSymbol(input[i + 1][j - 1])) isPart = true;
+					if (NotOutOfBoundsCheck.DownRight(i, j, input.Length, input[i].Length))
+						if (IsSymbol(input[i + 1][j + 1])) isPart = true;
 
 					if (num[0] == '.') num[0] = character;
 					else if (num[1] == '.') num[1] = character;
@@ -61,6 +59,167 @@ public sealed class Solution3 : ISolution
 
 	public string Part2(string filename)
 	{
-		throw new NotImplementedException();
+		var input = Input(filename);
+		List<int> ratios = [];
+		for (var i = 0; i < input.Length; i++)
+		{
+			var line = input[i].AsSpan();
+			for (var j = 0; j < line.Length; j++)
+			{
+				var character = input[i][j];
+				if (character == '*')
+				{
+					(var isGear, var n1, var n2) = IsGear(input, i, j);
+					if (isGear)
+					{
+						ratios.Add(n1 * n2);
+					}
+				}
+			}
+		}
+
+		return ratios.Sum().ToString();
+	}
+
+	static bool IsSymbol(char c) => !char.IsNumber(c) && c != '.';
+
+	static (bool, int, int) IsGear(Span<string> input, int i, int j)
+	{
+		var isGear = false;
+		int n1 = -1, n2 = -1;
+		var s1 = "";
+
+		if (NotOutOfBoundsCheck.Up(i) && char.IsNumber(input[i - 1][j]))
+		{
+			if (NotOutOfBoundsCheck.Left(j - 1)) s1 += input[i - 1][j - 2];
+			if (NotOutOfBoundsCheck.Left(j)) 
+			{
+				if (!char.IsNumber(input[i - 1][j - 1])) s1 = "";
+				s1 += input[i - 1][j - 1]; 
+			}
+			s1 += input[i - 1][j];
+			if (NotOutOfBoundsCheck.Right(j, input[0].Length)) s1 += input[i - 1][j + 1];
+			if (NotOutOfBoundsCheck.Right(j + 1, input[0].Length))
+				if (char.IsNumber(s1.Last())) s1 += input[i - 1][j + 2]; 
+			var ns = s1.Where(c => char.IsNumber(c));
+			if (ns.Any())
+				n1 = int.Parse(string.Join("", ns));
+			s1 = "";
+		}
+		if (NotOutOfBoundsCheck.Down(i, input.Length) && char.IsNumber(input[i + 1][j]))
+		{
+			if (NotOutOfBoundsCheck.Left(j - 1)) s1 += input[i + 1][j - 2];
+			if (NotOutOfBoundsCheck.Left(j)) 
+			{
+				if (!char.IsNumber(input[i + 1][j - 1])) s1 = "";
+				s1 += input[i + 1][j - 1]; 
+			}
+			s1 += input[i + 1][j];
+			if (NotOutOfBoundsCheck.Right(j, input[0].Length)) s1 += input[i + 1][j + 1];
+			if (NotOutOfBoundsCheck.Right(j + 1, input[0].Length)) 
+				if (char.IsNumber(s1.Last())) s1 += input[i + 1][j + 2];
+			var ns = s1.Where(c => char.IsNumber(c));
+			if (ns.Any())
+			{
+				if (n1 == -1)
+					n1 = int.Parse(string.Join("", ns));
+				else
+					n2 = int.Parse(string.Join("", ns));
+			}
+			s1 = "";
+		}
+		if (NotOutOfBoundsCheck.Left(j) && char.IsNumber(input[i][j - 1]) && n2 == -1)
+		{
+			if (NotOutOfBoundsCheck.Left(j - 2)) s1 += input[i][j - 3];
+			if (NotOutOfBoundsCheck.Left(j - 1)) s1 += input[i][j - 2];
+			s1 += input[i][j - 1];
+			var ns = s1.Where(c => char.IsNumber(c));
+			if (ns.Any())
+			{
+				if (n1 == -1)
+					n1 = int.Parse(string.Join("", ns));
+				else
+					n2 = int.Parse(string.Join("", ns));
+			}
+			s1 = "";
+		}
+		if (NotOutOfBoundsCheck.Right(j, input[i].Length) && char.IsNumber(input[i][j + 1]) && n2 == -1)
+		{
+			s1 += input[i][j + 1];
+			if (NotOutOfBoundsCheck.Right(j + 1, input[0].Length)) s1 += input[i][j + 2];
+			if (NotOutOfBoundsCheck.Right(j + 2, input[0].Length)) s1 += input[i][j + 3];
+			var ns = s1.Where(c => char.IsNumber(c));
+			if (ns.Any())
+			{
+				if (n1 == -1)
+					n1 = int.Parse(string.Join("", ns));
+				else
+					n2 = int.Parse(string.Join("", ns));
+			}
+			s1 = "";
+		}
+		if (NotOutOfBoundsCheck.UpLeft(i, j) && char.IsNumber(input[i - 1][j - 1]) && n2 == -1)
+		{
+			if (NotOutOfBoundsCheck.Left(j - 2)) s1 += input[i - 1][j - 3];
+			if (NotOutOfBoundsCheck.Left(j - 1)) s1 += input[i - 1][j - 2];
+			s1 += input[i - 1][j - 1];
+			var ns = s1.Where(c => char.IsNumber(c));
+			if (ns.Any())
+			{
+				if (n1 == -1)
+					n1 = int.Parse(string.Join("", ns));
+				else
+					n2 = int.Parse(string.Join("", ns));
+			}
+			s1 = "";
+		}
+		if (NotOutOfBoundsCheck.UpRight(i, j, input[i].Length) && char.IsNumber(input[i - 1][j + 1]) && n2 == -1)
+		{
+			s1 += input[i - 1][j + 1];
+			if (NotOutOfBoundsCheck.Right(j - 1, input[0].Length)) s1 += input[i - 1][j + 2];
+			if (NotOutOfBoundsCheck.Right(j - 2, input[0].Length)) s1 += input[i - 1][j + 3];
+			var ns = s1.Where(c => char.IsNumber(c));
+			if (ns.Any())
+			{
+				if (n1 == -1)
+					n1 = int.Parse(string.Join("", ns));
+				else
+					n2 = int.Parse(string.Join("", ns));
+			}
+			s1 = "";
+		}
+		if (NotOutOfBoundsCheck.DownLeft(i, j, input.Length) && char.IsNumber(input[i + 1][j - 1]) && n2 == -1)
+		{
+			if (NotOutOfBoundsCheck.Left(j - 2)) s1 += input[i + 1][j - 3];
+			if (NotOutOfBoundsCheck.Left(j - 1)) s1 += input[i + 1][j - 2];
+			s1 += input[i + 1][j - 1];
+			var ns = s1.Where(c => char.IsNumber(c));
+			if (ns.Any())
+			{
+				if (n1 == -1)
+					n1 = int.Parse(string.Join("", ns));
+				else
+					n2 = int.Parse(string.Join("", ns));
+			}
+			s1 = "";
+		}
+		if (NotOutOfBoundsCheck.DownRight(i, j, input.Length, input[i].Length) && char.IsNumber(input[i + 1][j + 1]) && n2 == -1)
+		{
+			s1 += input[i + 1][j + 1];
+			if (NotOutOfBoundsCheck.Right(j + 1, input[0].Length)) s1 += input[i + 1][j + 2];
+			if (NotOutOfBoundsCheck.Right(j + 2, input[0].Length)) s1 += input[i + 1][j + 3];
+			var ns = s1.Where(c => char.IsNumber(c));
+			if (ns.Any())
+			{
+				if (n1 == -1)
+					n1 = int.Parse(string.Join("", ns));
+				else
+					n2 = int.Parse(string.Join("", ns));
+			}
+			s1 = "";
+		}
+		if (n2 != -1) isGear = true;
+
+		return (isGear, n1, n2);
 	}
 }
