56051d4d5221bab20cdb5dbd6cb2480b00c56027
mnlcz <manuel.lopez.cosmitz@gmail.com>
2023-12-05 19:13:57 -0300

    Day4 OK

--- 2023/csharp/Day4/Day4.cs
+++ 2023/csharp/Day4/Day4.cs
@@ -4,28 +4,68 @@ namespace Day4;
 
 public sealed class Solution4 : ISolution
 {
-    private static string[][] Input(string filename) =>
-        new Parser("2023").IntoArrayMultiSeparator(filename, "\n", " | ");
+    private static string[][] Input(string filename)
+    {
+        var input = new Parser("2023").IntoArrayMultiSeparator(filename, "\n", " | ");
+        RemoveCardNumbers(input);
+
+        return input;
+    }
 
     public string Part1(string filename)
     {
-        List<int> points = [];
         var input = Input(filename);
-        RemoveCardNumbers(input);
 
-        foreach (var card in input)
-        {
-            var cWinners = WinningNumbers(card);
-            var cPoints = TotalPoints(cWinners);
-            points.Add(cPoints);
-        }
-
-        return points.Sum().ToString();
+        return TotalPoints(input).ToString();
     }
 
     public string Part2(string filename)
     {
-        throw new NotImplementedException();
+        var input = Input(filename);
+        var extraCopies = ExtraCopies(input);
+        var totalCopies = TotalCopies(extraCopies);
+
+        return totalCopies.Sum().ToString();
+    }
+
+    private static int[] TotalCopies(Span<int> extras)
+    {
+        List<int> copies = [];
+
+        foreach (var _ in extras) copies.Add(1);
+
+        for (var i = 0; i < extras.Length; i++)
+        {
+            var times = copies[i];
+            var extra = extras[i];
+            for (var j = 0; j < times; j++)
+            for (var k = 0; k < extra; k++)
+                copies[i + k + 1]++;
+        }
+
+        return [.. copies];
+    }
+
+    private static int[] ExtraCopies(Span<string[]> cards)
+    {
+        List<int> extraCopies = [];
+        foreach (var card in cards) extraCopies.Add(WinningNumbers(card).Length);
+
+        return [.. extraCopies];
+    }
+
+    private static int TotalPoints(Span<string[]> cards)
+    {
+        List<int> points = [];
+
+        foreach (var card in cards)
+        {
+            var cWinners = WinningNumbers(card);
+            var cPoints = CardPoints(cWinners);
+            points.Add(cPoints);
+        }
+
+        return points.Sum();
     }
 
     private static void RemoveCardNumbers(string[][] input)
@@ -51,7 +91,7 @@ public sealed class Solution4 : ISolution
         return [.. winningNumbers];
     }
 
-    private static int TotalPoints(Span<int> winningNumbers)
+    private static int CardPoints(Span<int> winningNumbers)
     {
         if (winningNumbers.Length == 0) return 0;
 
