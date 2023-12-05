33363f9d7524394cf6abc84463f535cdabda6b0b
mnlcz <manuel.lopez.cosmitz@gmail.com>
2023-12-05 19:22:42 -0300

    Day 5 generated with the script

---
+++ 2023/csharp/Day5/Day5.cs
@@ -0,0 +1,16 @@
+using Utils;
+
+namespace Day5;
+
+public sealed class Solution5 : ISolution
+{
+    public string Part1(string filename)
+    {
+        throw new NotImplementedException();
+    }
+
+    public string Part2(string filename)
+    {
+        throw new NotImplementedException();
+    }
+}
