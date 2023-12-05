8ed92297cef4c3d62024615d8f682c5f6235d036
mnlcz <manuel.lopez.cosmitz@gmail.com>
2023-12-05 00:25:59 -0300

    Day 4 Part 1 OK

--- 2023/README.md
+++ 2023/README.md
@@ -9,3 +9,4 @@ My advent of code solutions.
 |  1   |   ✔   |   ✔   | [Here](csharp/Day1/Day1.cs)  |
 |  2   |   ✔   |   ✔   | [Here](csharp/Day2/Day2.cs)  |
 |  3   |   ✔   |   ✔   | [Here](csharp/Day3/Day3.cs)  |
+|  4   |   ✔   |   ❌   | [Here](csharp/Day4/Day4.cs)  |
--- 2023/csharp/Day4/Day4.cs
+++ 2023/csharp/Day4/Day4.cs
@@ -4,13 +4,64 @@ namespace Day4;
 
 public sealed class Solution4 : ISolution
 {
+    private static string[][] Input(string filename) =>
+        new Parser("2023").IntoArrayMultiSeparator(filename, "\n", " | ");
+
     public string Part1(string filename)
     {
-        throw new NotImplementedException();
+        List<int> points = [];
+        var input = Input(filename);
+        RemoveCardNumbers(input);
+
+        foreach (var card in input)
+        {
+            var cWinners = WinningNumbers(card);
+            var cPoints = TotalPoints(cWinners);
+            points.Add(cPoints);
+        }
+
+        return points.Sum().ToString();
     }
 
     public string Part2(string filename)
     {
         throw new NotImplementedException();
+    }
+
+    private static void RemoveCardNumbers(string[][] input)
+    {
+        foreach (var c in input)
+        {
+            var clean = c[0].Split(": ")[1];
+            c[0] = clean;
+        }
+    }
+
+    private static int[] WinningNumbers(Span<string> card)
+    {
+        List<int> winningNumbers = [];
+        var winners = card[0].Split(" ").Where(i => i != "");
+        var candidates = card[1].Split(" ").Where(i => i != "");
+
+        foreach (var c in candidates)
+        foreach (var w in winners)
+            if (c == w)
+                winningNumbers.Add(int.Parse(c));
+
+        return [.. winningNumbers];
+    }
+
+    private static int TotalPoints(Span<int> winningNumbers)
+    {
+        if (winningNumbers.Length == 0) return 0;
+
+        var points = 1;
+
+        foreach (var _ in winningNumbers)
+        {
+            points *= 2;
+        }
+
+        return points / 2;
     }
 }
