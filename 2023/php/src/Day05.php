1f8a2e07a9d8f05a2fdebb302be5ddef9217187b
mnlcz <manuel.lopez.cosmitz@gmail.com>
2024-06-07 21:26:28 -0300

    D05P2 brute force implementation
    Works on the example. Takes too long on real input

--- 2023/php/src/Day05.php
+++ 2023/php/src/Day05.php
@@ -9,16 +9,6 @@ use Illuminate\Support\Str;
 
 class Day05
 {
-    public static function input(string $file): array
-    {
-        $in = Str::of(input($file))->explode(PHP_EOL.PHP_EOL);
-
-        $seeds = Str::of($in->first())->after('seeds: ')->explode(' ')->map(fn ($s) => intval($s))->toArray();
-        $rest = $in->skip(1)->map(fn ($s) => Str::trim($s))->map(fn ($s) => Str::of($s)->explode(PHP_EOL)->skip(1))->map(fn ($m) => $m->map(fn ($s) => Str::of($s)->explode(' ')->map(fn ($s) => intval($s))))->toArray();
-
-        return [array_values($seeds), array_values($rest)];
-    }
-
     public static function part1(bool $testing): int
     {
         [$seeds, $rest] = $testing ? static::input('5Sample1.txt') : static::input('5.txt');
@@ -30,6 +20,36 @@ class Day05
         }
 
         return min($out);
+    }
+
+    // FIXME: bruteforce solution takes too long. Works on the example tho
+    public static function part2(bool $testing): int
+    {
+        [$seeds, $rest] = $testing ? static::input('5Sample1.txt') : static::input('5.txt');
+        $seeds = $testing ? [79, 14, 55, 13] : $seeds; // Example given as a hint
+        $out = [];
+
+        for ($i = 0; $i < count($seeds); $i+=2) {
+            $start = $seeds[$i];
+            $len = $seeds[$i + 1];
+
+            while ($len-- > 0) {
+                $seed = $start + $len;
+                $out[] = static::cycle($rest, $seed);
+            }
+        }
+
+        return min($out);
+    }
+
+    public static function input(string $file): array
+    {
+        $in = Str::of(input($file))->explode(PHP_EOL.PHP_EOL);
+
+        $seeds = Str::of($in->first())->after('seeds: ')->explode(' ')->map(fn ($s) => intval($s))->toArray();
+        $rest = $in->skip(1)->map(fn ($s) => Str::trim($s))->map(fn ($s) => Str::of($s)->explode(PHP_EOL)->skip(1))->map(fn ($m) => $m->map(fn ($s) => Str::of($s)->explode(' ')->map(fn ($s) => intval($s))))->toArray();
+
+        return [array_values($seeds), array_values($rest)];
     }
 
     public static function cycle(array $maps, int $n): int
@@ -58,4 +78,5 @@ class Day05
     }
 }
 
-echo 'Part 1: '.Day05::part1(false).PHP_EOL;
+// echo 'Part 1: '.Day05::part1(false).PHP_EOL;
+echo 'Part 2: '.Day05::part2(false).PHP_EOL;
