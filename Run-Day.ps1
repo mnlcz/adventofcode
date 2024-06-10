3fa0fc567749e3e2f54d6726b260a49502f225bc
mnlcz <manuel.lopez.cosmitz@gmail.com>
2024-06-10 17:44:23 -0300

    Add support to php

--- Run-Day.ps1
+++ Run-Day.ps1
@@ -4,11 +4,25 @@ Param
     [String]$Year,
     
     [Parameter(Mandatory = $true, Position = 1)]
-    [String]$Day
+    [String]$Day,
+
+    [Parameter(Mandatory = $true, Position = 2)]
+    [String]$Lang
 )
 
 $Root = Get-Location
-$ProjName = "Day$Day"
-$Path = Join-Path $Root "$Year\csharp\$ProjName\$ProjName.csproj"
 
-dotnet run --project $Path
+if ($Lang.ToUpper() -eq "CS") {
+    $ProjName = "Day$Day"
+    $Path = Join-Path $Root "$Year\csharp\$ProjName\$ProjName.csproj"
+
+    dotnet run --project $Path
+} elseif ($Lang.ToUpper() -eq "PHP") {
+    $PhpPath = Join-Path $Root "$Year\php"
+    
+    Set-Location $PhpPath
+    
+    php ".\src\Day$Day.php"
+
+    Set-Location $Root
+}
--- Test-Day.ps1
+++ Test-Day.ps1
@@ -4,11 +4,28 @@ Param
     [String]$Year,
     
     [Parameter(Mandatory = $true, Position = 1)]
-    [String]$Day
+    [String]$Day,
+
+    [Parameter(Mandatory = $true, Position = 2)]
+    [String]$Lang
 )
 
 $Root = Get-Location
-$TestName = "TestingDay$Day"
-$Path = Join-Path $Root "$Year\csharp\$TestName\$TestName.csproj"
 
-dotnet test $Path
+if ($Lang.ToUpper() -eq "CS") {
+    $TestName = "TestingDay$Day"
+    $Path = Join-Path $Root "$Year\csharp\$TestName\$TestName.csproj"
+
+    dotnet test $Path
+} elseif($Lang.ToUpper() -eq "PHP") {
+    $PhpPath = Join-Path $Root "$Year\php"
+    
+    Set-Location $PhpPath
+    
+    $Pest = ".\vendor\bin\pest"
+    $TestPath = Join-Path $PhpPath "tests\Unit\Day${Day}Test.php"
+    
+    & $Pest $TestPath
+    
+    Set-Location $Root
+}
