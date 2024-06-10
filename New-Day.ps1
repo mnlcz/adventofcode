399aa7d3d9cb2ff96ab5d13bdc156c815c14ed3e
mnlcz <manuel.lopez.cosmitz@gmail.com>
2024-06-10 17:28:47 -0300

    Add language specification
    Now accepts a new parameter that defines the type of language, currently only "cs" and "php"

--- New-Day.ps1
+++ New-Day.ps1
@@ -4,26 +4,30 @@ Param
     [String]$Year,
     
     [Parameter(Mandatory = $true, Position = 1)]
-    [String]$Day
+    [String]$Day,
+
+    [Parameter(Mandatory = $true, Position = 2)]
+    [String]$Lang
 )
 
-$Root = Get-Location
-$Path = Join-Path $Root "$Year\csharp"
-$ProjName = "Day$Day"
-$TestName = "TestingDay$Day"
-$ProjPath = Join-Path $Path "$ProjName\$ProjName.csproj"
-$TestPath = Join-Path $Path "$TestName\$TestName.csproj"
-$UtilsPath = Join-Path $Root "utils\csharp\Utils\Utils.csproj"
+if ($Lang.ToUpper() -eq "CS") {
+    $Root = Get-Location
+    $Path = Join-Path $Root "$Year\csharp"
+    $ProjName = "Day$Day"
+    $TestName = "TestingDay$Day"
+    $ProjPath = Join-Path $Path "$ProjName\$ProjName.csproj"
+    $TestPath = Join-Path $Path "$TestName\$TestName.csproj"
+    $UtilsPath = Join-Path $Root "utils\csharp\Utils\Utils.csproj"
 
-Set-Location $Path
+    Set-Location $Path
 
-dotnet new console -o $ProjName
-dotnet new xunit -o $TestName
+    dotnet new console -o $ProjName
+    dotnet new xunit -o $TestName
 
-Set-Location $ProjName
-New-Item "$ProjName.cs"
+    Set-Location $ProjName
+    New-Item "$ProjName.cs"
 
-$Template = @"
+    $Template = @"
 using Utils;
 
 namespace $ProjName;
@@ -42,7 +46,7 @@ public sealed class Solution$Day : ISolution
 }
 "@
 
-$TestTemplate = @"
+    $TestTemplate = @"
 using $ProjName;
 
 namespace TestingDay$Day;
@@ -65,19 +69,87 @@ public class PartsTest
 }
 "@
 
-Add-Content -Path "$ProjName.cs" -Value $Template -Encoding utf8
+    Add-Content -Path "$ProjName.cs" -Value $Template -Encoding utf8
 
-Set-Location "$Path\$TestName"
-Remove-Item -Path "UnitTest1.cs"
-Add-Content -Path "PartsTest.cs" -Value $TestTemplate -Encoding utf8
+    Set-Location "$Path\$TestName"
+    Remove-Item -Path "UnitTest1.cs"
+    Add-Content -Path "PartsTest.cs" -Value $TestTemplate -Encoding utf8
 
-Set-Location $Root
+    Set-Location $Root
 
-dotnet sln add $ProjPath
-dotnet sln add $TestPath
+    dotnet sln add $ProjPath
+    dotnet sln add $TestPath
 
-dotnet add $ProjPath reference $UtilsPath
-dotnet add $TestPath reference $UtilsPath
-dotnet add $TestPath reference $ProjPath
+    dotnet add $ProjPath reference $UtilsPath
+    dotnet add $TestPath reference $UtilsPath
+    dotnet add $TestPath reference $ProjPath
 
-dotnet build
+    dotnet build
+} elseif ($Lang.ToUpper() -eq "PHP") {
+    $Root = Get-Location
+    $PhpPath = Join-Path $Root "$Year\php"
+    $SrcPath = Join-Path $PhpPath "\src"
+    $TestPath = Join-Path $PhpPath "\tests\Unit"
+    $Name = "Day$Day"
+    $Template = @"
+<?php
+
+namespace Aoc;
+
+use Illuminate\Support\Str;
+
+require 'vendor/autoload.php';
+require 'utils.php';
+
+class $Name
+{
+    public static function part1(bool `$testing): int
+    {
+        `$in = Str::of(input(`$testing ? '5Sample1.txt' : '5.txt'))->trim()->explode(PHP_EOL);
+
+        return PHP_INT_MIN;
+    }
+
+    public static function part2(bool `$testing): int
+    {
+        return PHP_INT_MIN;
+    }
+}
+
+// echo 'Part 1: '.$Name::part1(false).PHP_EOL;
+// echo 'Part 2: '.$Name::part2(false).PHP_EOL;
+"@
+
+    $TestTemplate = @"
+<?php
+
+describe('Part 1', function () {
+    test('with sample', function () {
+        expect(Aoc\$Name::part1(true))->toBe(PHP_INT_MIN);
+    });
+
+    test('with real input', function () {
+        expect(Aoc\$Name::part1(false))->toBe(PHP_INT_MIN);
+    });
+});
+
+describe('Part 2', function () {
+    test('with part of the sample', function () {
+        expect(Aoc\$Name::part2(true))->toBe(PHP_INT_MIN);
+    });
+
+    test('with real input', function () {
+        expect(Aoc\$Name::part2(false))->toBe(PHP_INT_MIN);
+    });
+});
+"@
+
+    Set-Location $SrcPath
+    Add-Content -Path "$Name.php" -Value $Template -Encoding utf8
+
+    Set-Location $TestPath
+    Add-Content -Path "${Name}Test.php" -Value $TestTemplate -Encoding utf8
+
+    Set-Location $Root
+}
+
