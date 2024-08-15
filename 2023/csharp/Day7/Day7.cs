6205e38869b59e9f7d50388541990f19319a32b1
mnlcz <manuel.lopez.cosmitz@gmail.com>
2024-08-15 02:01:55 -0300

    D07P2

--- 2023/csharp/Day7/Day7.cs
+++ 2023/csharp/Day7/Day7.cs
@@ -16,21 +16,23 @@ public sealed class Solution7 : ISolution
     {
         var input = Parse(filename);
 
-        return TotalWinning(input);
+        return TotalWinning(input, false);
     }
 
     public string Part2(string filename)
     {
-        throw new NotImplementedException();
+        var input = Parse(filename);
+
+        return TotalWinning(input, true);
     }
 
-    public string TotalWinning(string[][] rounds)
+    public string TotalWinning(string[][] rounds, bool jokers)
     {
         int w = 0;
         // Sort by Strongest() by Pair.X (hand)
         Array.Sort(rounds, (x, y) =>
         {
-            var s = Strongest(x[0], y[0]);
+            var s = Strongest(x[0], y[0], jokers);
 
             if (s == x[0])
                 return 1;
@@ -51,10 +53,10 @@ public sealed class Solution7 : ISolution
         return w.ToString();
     }
 
-    public string Strongest(string h1, string h2)
+    public string Strongest(string h1, string h2, bool jokers)
     {
-        var t1 = InterpretHand(h1);
-        var t2 = InterpretHand(h2);
+        var t1 = InterpretHand(h1, jokers);
+        var t2 = InterpretHand(h2, jokers);
 
         if (((int)t1) != ((int)t2))
         {
@@ -63,8 +65,8 @@ public sealed class Solution7 : ISolution
 
         for (var i = 0; i < h1.Length; i++)
         {
-            var c1 = _cardStrength[h1[i]];
-            var c2 = _cardStrength[h2[i]];
+            var c1 = CardStrength(jokers)[h1[i]];
+            var c2 = CardStrength(jokers)[h2[i]];
 
             if (h1[i] == h2[i])
                 continue;
@@ -75,11 +77,12 @@ public sealed class Solution7 : ISolution
         return h1; // !!
     }
 
-    public HandType InterpretHand(string hand)
+    public HandType InterpretHand(string ogHand, bool joker)
     {
         List<int> cs = new();
+        var hand = !joker ? ogHand : ReinterpretHand(ogHand);
 
-        foreach (var i in _cardStrength.Keys)
+        foreach (var i in CardStrength(joker).Keys)
         {
             var count = hand.Count(h => h == i);
 
@@ -104,23 +107,71 @@ public sealed class Solution7 : ISolution
         return HandType.HighCard;
     }
 
-    // Lower = Stronger
-    private readonly Dictionary<char, int> _cardStrength = new()
+    public string ReinterpretHand(string og)
     {
-        { 'A', 1 },
-        { 'K', 2 },
-        { 'Q', 3 },
-        { 'J', 4 },
-        { 'T', 5 },
-        { '9', 6 },
-        { '8', 7 },
-        { '7', 8 },
-        { '6', 9 },
-        { '5', 10 },
-        { '4', 11 },
-        { '3', 12 },
-        { '2', 13 },
-    };
+        if (!og.Contains('J')) 
+            return og;
+        
+        var reps = og.GroupBy(c => c).ToDictionary(g => g.Key, g => g.Count());
+        var jokers = reps['J'];
+        var replaceJoker = (int n) => og.Replace('J', reps.First(x => x.Value == n && x.Key != 'J').Key);
+
+        switch (jokers)
+        {
+            case 5:
+                return og;
+            case 4:
+                return replaceJoker(1);
+            case 3:
+                return reps.Any(r => r.Value == 2) ? replaceJoker(2) : replaceJoker(1);
+            case 2:
+                if (reps.Any(r => r.Value == 3)) return replaceJoker(3);
+                else if (reps.Any(r => r.Value == 2 && r.Key != 'J')) return replaceJoker(2);
+                else return replaceJoker(1);
+            default:
+                if (reps.Any(r => r.Value == 4)) return replaceJoker(4);
+                else if (reps.Any(r => r.Value == 3)) return replaceJoker(3);
+                else if (reps.Any(r => r.Value == 2)) return replaceJoker(2);
+                else return replaceJoker(1);
+        }
+    }
+
+    // Lower = Stronger
+    private Dictionary<char, int> CardStrength(bool joker)
+    {
+        if (!joker) return new()
+        {
+            { 'A', 1 },
+            { 'K', 2 },
+            { 'Q', 3 },
+            { 'J', 4 },
+            { 'T', 5 },
+            { '9', 6 },
+            { '8', 7 },
+            { '7', 8 },
+            { '6', 9 },
+            { '5', 10 },
+            { '4', 11 },
+            { '3', 12 },
+            { '2', 13 },
+        };
+        else return new()
+        {
+            { 'A', 1 },
+            { 'K', 2 },
+            { 'Q', 3 },
+            { 'T', 4 },
+            { '9', 5 },
+            { '8', 6 },
+            { '7', 7 },
+            { '6', 8 },
+            { '5', 9 },
+            { '4', 10 },
+            { '3', 11 },
+            { '2', 12 },
+            { 'J', 13 },
+        };
+    }
 
     // Lower = Stronger
     public enum HandType
