a8e60703cc744352afa97428fbea88e5dc52b5f1
MnLCz <mnlcz@tuta.io>
2026-02-02 17:01:40 -0300

    2025D1P2

--- 2025/php/src/01.php
+++ 2025/php/src/01.php
@@ -22,6 +22,7 @@ const MIN = 0;
 const SIZE = 100;
 
 part1($rotations);
+part2($rotations);
 
 function part1(array &$input): void
 {
@@ -45,7 +46,37 @@ function part1(array &$input): void
     echo "Part 1: $count\n";
 }
 
+function part2(array &$input): void
+{
+    $dial = 50;
+    $count = 0;
+    foreach ($input as $rotation) {
+        $direction = substr($rotation, 0, 1);
+        $n = substr($rotation, 1);
+
+        if ($direction == "L") {
+            $n = -$n;
+        }
+
+        $result = $dial + $n;
+
+        if ($result == 0) {
+            $count++;
+        } elseif ($result > 0) {
+            $count += intdiv($result, SIZE);
+        } elseif ($result < 0) {
+            $count += intdiv(abs($result) + SIZE, SIZE);
+            if ($dial == 0) {
+                $count--;
+            }
+        }
+
+        $dial = wrap($result);
+    }
+    echo "Part 2: $count\n";
+}
+
 function wrap(int $num): int
 {
-    return (($num - MIN) % SIZE) + MIN;
+    return (((($num - MIN) % SIZE) + SIZE) % SIZE) + MIN;
 }
